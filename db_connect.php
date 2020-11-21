aca68ecedade390382ea4796da26342c17147cfb
saileela <65408239+saileela1998@users.noreply.github.com>
2020-11-21 22:36:37 +0530

    Add files via upload

---
+++ db_connect.php
@@ -0,0 +1,6 @@
+<?php 
+	$conn=mysqli_connect("localhost","id15464646_root", "KUMMETHAsaileela123@#","id15464646_bank2");
+	if(!$conn) {
+		echo "Connection Error: " . mysqli_connect_error();
+	}
+?>
---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Banking System</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+    <?php include('navbar.php'); ?>
+
+    <section>
+        <div class="container">
+        <div class="row">
+            <div class="col s6 description">
+                <p><h3>Welcome to</h3>
+                <h1>Banking System</h1>
+                </p>
+            </div>
+            <div class="col s6">
+                <img src="sbg.png" class="vector" width="500px" height="500px">
+            </div>
+        
+        <div class="go-to">
+        	<a href="transferHistory.php" class="waves-effect waves-light btn-large black z-depth-2">Transfer History</a>
+        	<a href="selectUser.php" class="waves-effect waves-light btn-large black z-depth-2">Select User</a>
+        </div>
+    </div>
+    
+    </section>
+
+</body>
+
+</html>
